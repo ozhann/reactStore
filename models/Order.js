3766a1dd414f0699adac9a32c2d9bff9be18f2aa
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-02-27 12:37:37 +0100

    first v of User and Product model

---
+++ models/Order.js
@@ -0,0 +1,6 @@
+// not sure if it is needed
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const Order = mongoose.model("order", orderSchema);
+// module.exports = Order;
--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,31 @@
 /* We'll write the schema and register our model for the posts here */
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: { type: String, require: true },
+  category: {
+    type: String,
+    enum: ["Collars", "Leashes", "Beds", "Toys", "Bowls", "Food", "Apparel"]
+  },
+  descriptionShort: { type: String, minlength: 15 },
+  descriptionFull: { type: String, minlength: 50 },
+  price: {
+    type: Currency,
+    required: true
+  },
+  onStock: {
+    type: number
+  },
+  images: {
+    type: Array,
+    default: "public/images/dogpaw.png"
+  },
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
