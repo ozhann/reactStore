cf2ebfaa9d8f918c1e8bb9fe0c79181c07850917
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-03-05 17:23:59 +0100

    deploy

---
+++ client/build/precache-manifest.f7579b9f9251d2b17b5a48505f85d61b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44fb679bac07fb18c524f80745aa719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99083a554a13f3d859a9",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "f95d80f3d9a5f76f419c",
+    "url": "/static/css/main.b1817720.chunk.css"
+  },
+  {
+    "revision": "99083a554a13f3d859a9",
+    "url": "/static/js/2.8aae0945.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.8aae0945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f95d80f3d9a5f76f419c",
+    "url": "/static/js/main.3d73825b.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
